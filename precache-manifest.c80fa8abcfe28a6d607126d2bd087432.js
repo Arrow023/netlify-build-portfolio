10ff404f1bb8902fa8c3a3ab5292d64503f33880
arrow023 <piyushchohan48@gmail.com>
2023-07-01 19:56:22 +0530

    Ver1.3

---
+++ precache-manifest.c80fa8abcfe28a6d607126d2bd087432.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = [
+  {
+    "revision": "0ca8008a01f5b56939ce",
+    "url": "/static/css/main.80e9c525.chunk.css"
+  },
+  {
+    "revision": "0ca8008a01f5b56939ce",
+    "url": "/static/js/main.0ca8008a.chunk.js"
+  },
+  {
+    "revision": "1353eab83774f5a5b410",
+    "url": "/static/css/1.de04bacb.chunk.css"
+  },
+  {
+    "revision": "1353eab83774f5a5b410",
+    "url": "/static/js/1.1353eab8.chunk.js"
+  },
+  {
+    "revision": "229c360febb4351a89df",
+    "url": "/static/js/runtime~main.229c360f.js"
+  },
+  {
+    "revision": "b796ccb8aa46f6fe32a870b4ff1ed5d2",
+    "url": "/static/media/profilepic.b796ccb8.jpg"
+  },
+  {
+    "revision": "f3976f622c980244a89f7936c839cad4",
+    "url": "/index.html"
+  }
+];
